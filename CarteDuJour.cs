2a7b381761f37a382bf916b2a616eaed76528e53
jovalnik <jovalnik@gmx.com>
2021-09-26 11:33:02 +0200

    blev inte så mkt om klassarv

---
+++ CarteDuJour.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace MainMenu
+{
+    public class CarteDuJour : Menu
+    {
+        public int ALaCarte()
+        {
+            Console.CursorVisible = false;
+            int choice = 0;
+                        
+            while (true)
+            {
+            
+                int max = MenuOptions.Count;
+               
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine(Title);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine(" ");
+
+                for (int i = 0; i < max; i++)
+                {
+
+                    if (i == choice)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("[" + (i + 1) + "] " + MenuOptions[i]);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("[" + (i + 1) + "] " + MenuOptions[i]);
+                    }
+                }
+
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.Black;
+                
+                ConsoleKey readKey = Console.ReadKey().Key;
+                if (readKey == ConsoleKey.DownArrow)
+                {
+                    choice++;
+                    choice = choice % max;
+
+                }
+
+                else if (readKey == ConsoleKey.UpArrow)
+                {
+                    if (choice == 0)
+                    {
+                        choice = max - 1;
+                    }
+                    else
+                    {
+                        choice--;
+                    }
+                }
+                else
+
+                if (readKey == ConsoleKey.Enter)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    return choice;                     
+                }
+                
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;              
+            }
+        }
+        public override string GetOption(int choice)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Red;
+            string returnMe = MenuOptions[choice];
+            returnMe = returnMe.ToUpper();
+            return returnMe;
+        }
+    }
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MainMenu
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+
+            CarteDuJour carte = new CarteDuJour();
+
+            carte.AddOption("Filet Mignon");
+            carte.AddOption("Homard a la Amoricaine");
+            carte.AddOption("Just a tiny mint wafer, Monsieur");
+            carte.Title = "Welcome to RoboGarcon (Use arrow keys to select, <ENTER> to choose)  ";
+
+            int choice = carte.ALaCarte();
+
+            Console.WriteLine(" ");
+            Console.WriteLine("You chose " + carte.GetOption(choice));
+           
+            Console.ReadLine();          
+        }
+    }
+    
+
+}
