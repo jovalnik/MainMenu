6f8ccf4b45ddcb123eb0630dff08c134590b2757
jovalnik <jovalnik@gmx.com>
2021-09-26 21:54:00 +0200

    gjorde så att min klass funktioner är overrides.

--- CarteDuJour.cs
+++ CarteDuJour.cs
@@ -1,48 +1,59 @@
 ﻿using System;
+using System.Threading;
 
 namespace MainMenu
 {
     public class CarteDuJour : Menu
+   //public class CarteDuJour 
     {
-        public int ALaCarte()
+        int choice;
+
+        public override void ShowMenu()
         {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(Title);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(" ");
             Console.CursorVisible = false;
-            int choice = 0;
-                        
-            while (true)
+            int max = MenuOptions.Count;
+
+            for (int i = 0; i < max; i++)
             {
-            
-                int max = MenuOptions.Count;
-               
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.WriteLine(Title);
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.WriteLine(" ");
 
-                for (int i = 0; i < max; i++)
+
+                if (i == choice)
                 {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("[" + (i + 1) + "] " + MenuOptions[i] );
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
 
-                    if (i == choice)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.BackgroundColor = ConsoleColor.Blue;
-                        Console.WriteLine("[" + (i + 1) + "] " + MenuOptions[i]);
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.BackgroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("[" + (i + 1) + "] " + MenuOptions[i]);
-                    }
+                    Console.WriteLine("[" + (i + 1) + "] " + MenuOptions[i] );
                 }
 
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.BackgroundColor = ConsoleColor.Black;
-                
+                Console.ForegroundColor = ConsoleColor.White;
+                 Console.BackgroundColor = ConsoleColor.Black;
+            }
+
+
+        }
+
+        public override int LetUserChoose()
+        {
+            int max = MenuOptions.Count;
+            
+
+                //choice = 2;
+
                 ConsoleKey readKey = Console.ReadKey().Key;
+                //Thread.Sleep(50);
                 if (readKey == ConsoleKey.DownArrow)
                 {
                     choice++;
@@ -66,13 +77,16 @@ namespace MainMenu
                 if (readKey == ConsoleKey.Enter)
                 {
                     Console.ForegroundColor = ConsoleColor.White;
-                    return choice;                     
+                    return choice;
                 }
-                
-                Console.Clear();
-                Console.ForegroundColor = ConsoleColor.White;              
-            }
+               
+            
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("noneOFtheAbove");
+            return max + 1;
         }
+
         public override string GetOption(int choice)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -80,6 +94,11 @@ namespace MainMenu
             string returnMe = MenuOptions[choice];
             returnMe = returnMe.ToUpper();
             return returnMe;
+        }
+        public int GetLength ()
+        {
+            int length= MenuOptions.Count;
+            return length;
         }
     }
     
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace MainMenu
 {
@@ -14,11 +15,22 @@ namespace MainMenu
             carte.AddOption("Just a tiny mint wafer, Monsieur");
             carte.Title = "Welcome to RoboGarcon (Use arrow keys to select, <ENTER> to choose)  ";
 
-            int choice = carte.ALaCarte();
-
-            Console.WriteLine(" ");
-            Console.WriteLine("You chose " + carte.GetOption(choice));
-           
+            //int choice = carte.ALaCarte();
+            while (true)
+            {
+                int choice = 5;
+                int max = carte.GetLength();
+                carte.ShowMenu();
+                //Thread.Sleep(5500);
+                //int choice = carte.LetUserChoose();
+                choice = carte.LetUserChoose();
+                if (choice < max)
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine("You chose " + carte.GetOption(choice));
+                    break;
+                }
+            }
             Console.ReadLine();          
         }
     }
